f1485d45d90e78b16a3a572616b4bbf4d40870f6
nikhil5589 <149615420+nikhil5589@users.noreply.github.com>
2023-11-04 22:40:38 +0530

    original app
    original

--- app.py
+++ app.py
@@ -2,27 +2,29 @@ import streamlit as st
 import pickle
 import numpy as np
 
-regressor = pickle.load(open('regressor.pkl', 'rb'))
-laptop = pickle.load(open('laptoporig.pkl', 'rb'))
+pipe = pickle.load(open('pipe.pkl', 'rb'))
+df = pickle.load(open('df1.pkl', 'rb'))
 
 st.title("Laptop price predictor")
 
-Company = st.selectbox('Brand', laptop['Company'].unique())
-TypeName = st.selectbox('Type', laptop['TypeName'].unique())
-Inches = st.selectbox('Inches', laptop['Inches'].unique())
-Cpu = st.selectbox('Cpu', laptop['Cpu'].unique())
-Memory = st.selectbox('Memory', laptop['Memory'].unique())
-Gpu = st.selectbox('Gpu', laptop['Gpu'].unique())
-OpSys = st.selectbox('OpSys', laptop['OpSys'].unique())
-Weight = st.selectbox('Weight', laptop['Weight'].unique())
+Company = st.selectbox('Brand', df['Company'].unique())
+TypeName = st.selectbox('Type', df['TypeName'].unique())
+Inches = st.selectbox('Inches', df['Inches'].unique())
 Ram = st.selectbox('RAM(in GB)', [2, 4, 6, 8, 10, 12, 16, 24, 32, 64])
+Weight = st.selectbox('Weight', df['Weight'].unique())
 Touchscreen = st.selectbox('Touchscreen', ['No', 'Yes'])
-Ips = st.selectbox('Ips', laptop['Ips'].unique())
-selected_features = ['Company', 'TypeName', 'Inches', 'Cpu', 'Ram', 'Memory', 'Gpu', 'OpSys', 'Weight', 'Touchscreen',
-                     'Ips']
+Ips = st.selectbox('Ips	', df['Ips'].unique())
+gpu = st.selectbox('Gpu', df['gpu'].unique())
+cpu = st.selectbox('Cpu', df['cpu'].unique())
+HDD	 = st.selectbox('HDD', df['HDD'].unique())
+SSD = st.selectbox('SSD', df['SSD'].unique())
+Hybrid =  st.selectbox('Hybrid',df['Hybrid'].unique())
+Flash_Storage = st.selectbox('Flash_Storage', df['Flash_Storage'].unique())
+os = st.selectbox('os', df['os'].unique())
+selected_features =['Company', 'TypeName', 'Inches', 'Ram', 'Weight', 'Touchscreen', 'Ips', 'gpu', 'cpu', 'HDD', 'SSD', 'Hybrid', 'Flash_Storage', 'os']
 
 # Create a subset of the DataFrame with only the selected columns
-input_data = laptop[selected_features]
+input_data = df[selected_features]
 
 # Now, you can use the selected input data for prediction
 if st.button('Predict Price'):
@@ -31,11 +33,16 @@ if st.button('Predict Price'):
     else:
         Touchscreen = 0
 
-    # You should pass only the selected features for prediction
-    query = np.array([Company, TypeName, Inches, Cpu, Ram, Memory, Gpu, OpSys, Weight, Touchscreen, Ips])
-    query = query.reshape(1, 11)
+        if Ips == 'Yes':
+            Ips = 1
+        else:
+            Ips = 0
 
-    predicted_price = regressor.predict(query)
+            # You should pass only the selected features for prediction
+    query = np.array([Company, TypeName, Inches, Ram, Weight, Touchscreen, Ips, gpu, cpu, HDD, SSD, Hybrid, Flash_Storage, os])
+    query = query.reshape(1, 14)
+
+    predicted_price =np.exp(pipe.predict(query))
 
     print("query:", query)
 
